99b3e37a3e59fb2b6ec607a4103abaebde2574c6
Marvellous Ogunleke <marvellousabiola08@gmail.com>
2025-06-25 21:41:02 +0100

    Fix folder casing: Components → components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,6 +13,7 @@ const About = () => {
   const handleCloseVideo=()=>(
     setIsVideoPlaying(false)
   )
+  const apiUrl=import.meta.env.VITE_VIDEOLINK
   return (
     <div id='about' className='bg-[#f7f8fc] pb-16 pt-20'>
     <motion.div 
@@ -53,20 +54,20 @@ const About = () => {
     </motion.div>
     {
       isVideoPlaying &&(
-        <div className='fixed insert-0 bg-black bg-opacity-75 flex items-center justify-center z-50'>
-          <div className='relative w-full h-full flex item-center justify-center'>
+        <div className='fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'>
+          <div className='relative w-full h-full flex items-center justify-center'>
           <iframe
-          width='560'
-          height='315'
-          src='https://www.youtube.com/marvellousogun'
-          title='Youtube Video Player'
-          frameBorder='0'
-          allow='accelerator; autoplay;clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          allowFullScreen
-          className='rounded-lg'
-          >
-          </iframe>
-          <button onClick={handleCloseVideo} className='absolute top-4 right-4 text-white text-2xl cursor-picture'>
+  width="560"
+  height="315"
+  src={apiUrl}
+  title="YouTube video player"
+  frameBorder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  referrerPolicy="strict-origin-when-cross-origin"
+  allowFullScreen
+  className="rounded-lg"
+/>
+          <button onClick={handleCloseVideo} className='absolute top-4 right-4 text-white text-2xl cursor-pointer'>
             x
           </button>
         </div>
