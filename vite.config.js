b7d5d6400e980611260e8a62421d9ed3f1fbd0df
Marvellous Ogunleke <marvellousabiola08@gmail.com>
2025-11-02 14:35:22 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    exclude: ['prop-types'], // helps avoid Windows/prop-types optimizer issue
+  },
   build: {
-    minify: false,
+    minify: false, // optional (can turn true in production)
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
   },
 })
